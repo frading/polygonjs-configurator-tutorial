643c915fe614ad6ae31f951adb45c06dbb4a1f17
Guillaume Fradin <guillaumefradin@gmail.com>
2021-05-05 22:31:37 +0100

    part 5 completed - progress bar

--- src/polygonjs/scenes/shelf_configurator/setupScene.js
+++ src/polygonjs/scenes/shelf_configurator/setupScene.js
@@ -16,4 +16,30 @@ export async function setupScene(data) {
 	document.getElementById('geo1-line1-pointsCount').addEventListener('input', function(event){
 		scene.node('/geo1/line1').p.pointsCount.set(event.target.value);
 	})
+
+	let assetsLoadedCount = 0;
+	const maxAssetsCount = 2;
+	scene.node('/EVENT/nodeCook1').onDispatch('each', ()=>{
+		assetsLoadedCount++;
+		const progress = assetsLoadedCount / maxAssetsCount;
+		updateProgressBar(progress)
+	})
+}
+
+
+// this assumes a progress value going from 0 to 1.
+function updateProgressBar(progress){
+	if(progress < 1){
+		document.getElementById('polygonjs-progress').style.width = `${Math.round(progress*100)}%`;
+		document.getElementById('polygonjs-progress-bar-label').innerText = `${Math.round(progress*100)}%`;
+	} else {
+		// when we reach 1, we can remove the progress bar and fade out the poster
+		const container = document.getElementById('polygonjs-progress-bar-container');
+		container.parentElement.removeChild(container);
+		const poster = document.getElementById('polygonjs-loading-poster');
+		poster.classList.add('fade-out');
+		setTimeout(()=>{
+			poster.parentElement.removeChild(poster);
+		},1200);
+	}
 }
